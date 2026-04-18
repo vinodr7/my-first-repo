b9c4be0294f23f530ecbc23ec786def8fe4180f7
vinodr7 <101313296+vinodr7@users.noreply.github.com>
2026-04-18 23:50:18 +0530

    #updated to point venv

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello fromm D: driver setup!")
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init()
+
+print(Fore.CYAN + "Hello from my D: drive setup!" + Style.RESET_ALL)
+print(Fore.GREEN + "Virtual environment is working!" + Style.RESET_ALL)
